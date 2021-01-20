913382b4918be531b101a786a7e121eee037ce76
Amir Kalmoni <amirkalmoni@gmail.com>
2021-01-20 12:37:54 -0500

    link btwn Cognito User and user frm dtbse

--- App.tsx
+++ App.tsx
@@ -1,18 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 import { withAuthenticator } from "aws-amplify-react-native";
 import Amplify from "aws-amplify";
 import config from "./src/aws-exports.js";
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const randomImage = [
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/2.jpg',
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/3.jpg',
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg',
+    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/5.jpg'
+  ]
+
+const getRandomImage = ()=>{
+return randomImage[Math.floor(Math.random()*randomImage.length)];}
+  
+
+  // this part is only run when the appplication is mounted for the first time
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get authenticated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+        //get the user from the backend with the user id from auth
+        //if there is no user in the db with the id, create one
+        console.log(userData);
+        if (userData.data.getUser) {
+          console.log("user already registered in database");
+        }
+        const newUser ={
+          id:userInfo.attributes.sub,
+          name:userInfo.username,
+          imageURI:getRandomImage(),
+          status: 'Hey, I am using Whatsapp',
+        }
+        await API.graphql(graphqlOperation(createUser,{input:newUser}))
+      }
+    };
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
